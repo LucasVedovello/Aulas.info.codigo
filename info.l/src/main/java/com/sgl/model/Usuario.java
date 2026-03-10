eaf016bf2089d61195e64cc990ca2efc1d14c391
LucasVedovello <lucasscaranovedovello@gmail.com>
2026-03-10 08:49:41 -0300

    Add files via upload

---
+++ info.l/src/main/java/com/sgl/model/Usuario.java
@@ -0,0 +1,25 @@
+package main.java.com.sgl.model;
+
+public class Usuario {
+    private String nome;
+    private Int IdUsuario;
+    
+    public Usuario(String nome, Int IdUsuario){
+        this.nome = nome;
+        this.IdUsuario = IdUsuario;
+
+    }
+    public String nome getNome;
+
+
+
+
+
+
+
+
+
+
+
+
+}
